c27a0af014be93142472d7b72cec9100b923e9d0
shainkitwr <shainkitwr173@gmail.com>
2020-02-12 19:35:17 +0530

    FundooFrontend

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
 
