7de4dcac7e572972176e15e41ee4a6ae86362998
shainkitwr <shainkitwr173@gmail.com>
2020-02-13 19:40:55 +0530

    FundooFrontend

---
+++ src/app/dashboard/notes/updatenote/updatenote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-updatenote',
+  templateUrl: './updatenote.component.html',
+  styleUrls: ['./updatenote.component.scss']
+})
+export class UpdatenoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
