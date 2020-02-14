14f6f1130734c91bb360c9bfa72f976823da7211
shainkitwr <shainkitwr173@gmail.com>
2020-02-14 19:28:34 +0530

    FundooFrontend

---
+++ src/app/model/note.model.ts
@@ -0,0 +1,10 @@
+export class Note {
+    title:String;
+    content:String;
+    is_deleted:boolean;
+    is_pinned:boolean;
+    is_archived:boolean;
+    note_color:String;
+    reminder:String;
+    user_id:String;
+}
