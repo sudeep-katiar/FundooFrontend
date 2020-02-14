c38be401cf206131ba80c89600f9a3994ad6882d
shainkitwr <shainkitwr173@gmail.com>
2020-02-14 19:32:22 +0530

    FundooFrontend

--- src/app/dashboard/notes/createnote/createnote.component.ts
+++ src/app/dashboard/notes/createnote/createnote.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NoteserviceService } from 'src/app/service/noteservice.service';
+import { Note } from 'src/app/model/note.model';
 
 @Component({
   selector: 'app-createnote',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createnote.component.scss']
 })
 export class CreatenoteComponent implements OnInit {
-
-  constructor() { }
-
+  popup:boolean=false;
+  constructor(private router:Router,private noteservice: NoteserviceService, private snackbar:MatSnackBar ) { }
+  note:Note = new Note();
+  Token=localStorage.getItem('token')
+  
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if(this.note.title) {
+      this.noteservice.addNote(this.note, this.Token).subscribe(notes=> {
+        this.note = new Note();
+        console.log(this.note);
+        this.snackbar.open('Note Created', 'OK', {duration:3000});
+      },
+      (error: any) => {
+        this.snackbar.open(error.error.description, 'error', {duration:3000});
+      });
+    }
+  }
+
+  onPopup() {
+    this.popup=true;
   }
 
 }
