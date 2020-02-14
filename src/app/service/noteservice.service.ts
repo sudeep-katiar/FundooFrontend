d6c01c757c2de56f0bb8e5f5841ae62112c33d07
shainkitwr <shainkitwr173@gmail.com>
2020-02-14 19:30:06 +0530

    FundooFrontend

---
+++ src/app/service/noteservice.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteserviceService {
+  private noteURl=environment.notesApiUrl;
+  private httpOptions = {
+    headers: new HttpHeaders({'Content-Type':'application/json'})
+  };
+
+  constructor(private http:HttpClient) { }
+
+  addNote(Note:any, token:string):Observable<any>{
+    return this.http.post<any>(this.noteURl + environment.createNoteURL + token,Note);
+  }
+
+}
