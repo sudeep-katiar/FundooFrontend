976453a8862811b74d968ea627b6cdd174412350
shainkitwr <shainkitwr173@gmail.com>
2020-02-17 19:31:12 +0530

    FundooFrontend

--- src/app/service/noteservice.service.ts
+++ src/app/service/noteservice.service.ts
@@ -23,11 +23,48 @@ export class NoteserviceService {
     return this.http.post<any>(this.noteURl + environment.displayNoteURL + token,Note);
   }
 
+  deleteNote(token:string,id:any):Observable<any>{
+    return this.http.delete<any>(this.noteURl+"notes/delete/"+id,{headers: new HttpHeaders().set('token',  token) })
+  }
+
   updateNote(Note: any, token: string, noteId: Number): Observable<any> {
     console.log(noteId);
-
     return this.http.post<any>(this.noteURl + environment.updateNoteURL + token, Note)
-
   }
+
+  getTrashedNotes(token:string):Observable<any>{
+    return this.http.get<any>(this.noteURl+"trashednotes",{headers: new HttpHeaders().set('token',  token) })
+  }
+
+  getArchivedNotes(token:string):Observable<any>{
+    return this.http.get<any>(this.noteURl+"archievednotes",{headers: new HttpHeaders().set('token',  token) })
+  }
+
+  pinNotes(token:string,id:any):Observable<any>{
+
+    return this.http.post<any>(this.noteURl+"notes/pinned/"+id,{headers: new HttpHeaders().set('token',  token) })
+     
+  }
+  unPinNotes(token:string,id:any):Observable<any>{
+
+    return this.http.post<any>(this.noteURl+"notes/unpinned/"+id,{headers: new HttpHeaders().set('token',  token) })
+  }
+
+  archieveNote(token:string,id:any):Observable<any>{
+    return this.http.post<any>(this.noteURl+"notes/archieve/"+id,{headers: new HttpHeaders().set('token',  token) })
+  }
+
+  unarchieveNote(token:string,id:any):Observable<any>{
+    return this.http.post<any>(this.noteURl+"notes/unarchieve/"+id,{headers: new HttpHeaders().set('token',  token) })
+  }
+
+  trashnote(token:string,id:any):Observable<any>{
+    return this.http.post<any>(this.noteURl+"notes/trashed/"+id,{headers: new HttpHeaders().set('token',  token) })
+  }
+
+  restoreNote(token:string,id:any):Observable<any>{
+    return this.http.post<any>(this.noteURl+"notes/restore/"+id,{headers: new HttpHeaders().set('token',  token) })
+  }
+
 
 }
