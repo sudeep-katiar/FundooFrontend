787c9df5db71ef2af06ca64a1017c3ed652a26b2
shainkitwr <shainkitwr173@gmail.com>
2020-02-20 19:05:28 +0530

    FundooFrontend

--- src/app/model/note.model.ts
+++ src/app/model/note.model.ts
@@ -1,5 +1,5 @@
 export class Note {
-    id:number;
+    id:string;
     title:string;
     content:string;
     is_deleted:boolean;
--- src/app/service/noteservice.service.ts
+++ src/app/service/noteservice.service.ts
@@ -23,47 +23,9 @@ export class NoteserviceService {
     return this.http.post<any>(this.noteURl + environment.displayNoteURL + token,Note);
   }
 
-  deleteNote(token:string,id:any):Observable<any>{
-    return this.http.delete<any>(this.noteURl+"notes/delete/"+id,{headers: new HttpHeaders().set('token',  token) })
-  }
-
   updateNote(Note: any, token: string, id: any): Observable<any> {
     console.log(id);
-    return this.http.post<any>(this.noteURl + environment.updateNoteURL + token,{headers: new HttpHeaders().set('id', id) }, Note)
-  }
-
-  getTrashedNotes(token:string):Observable<any>{
-    return this.http.get<any>(this.noteURl+"trashednotes",{headers: new HttpHeaders().set('token',  token) })
-  }
-
-  getArchivedNotes(token:string):Observable<any>{
-    return this.http.get<any>(this.noteURl+"archievednotes",{headers: new HttpHeaders().set('token',  token) })
-  }
-
-  pinNotes(token:string,id:any):Observable<any>{
-
-    return this.http.post<any>(this.noteURl+"notes/pinned/"+id,{headers: new HttpHeaders().set('token',  token) })
-     
-  }
-  unPinNotes(token:string,id:any):Observable<any>{
-
-    return this.http.post<any>(this.noteURl+"notes/unpinned/"+id,{headers: new HttpHeaders().set('token',  token) })
-  }
-
-  archieveNote(token:string,id:any):Observable<any>{
-    return this.http.post<any>(this.noteURl+"notes/archieve/"+id,{headers: new HttpHeaders().set('token',  token) })
-  }
-
-  unarchieveNote(token:string,id:any):Observable<any>{
-    return this.http.post<any>(this.noteURl+"notes/unarchieve/"+id,{headers: new HttpHeaders().set('token',  token) })
-  }
-
-  trashnote(token:string,id:any):Observable<any>{
-    return this.http.post<any>(this.noteURl+"notes/trashed/"+id,{headers: new HttpHeaders().set('token',  token) })
-  }
-
-  restoreNote(token:string,id:any):Observable<any>{
-    return this.http.post<any>(this.noteURl+"notes/restore/"+id,{headers: new HttpHeaders().set('token',  token) })
+    return this.http.post<any>(this.noteURl + environment.updateNoteURL + token,Note,{headers: new HttpHeaders().set('id', id) });
   }
 
 
