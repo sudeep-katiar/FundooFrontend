12003186b057eaf6cca423db8fdda56d495ab6a5
shainkitwr <shainkitwr173@gmail.com>
2020-02-27 19:34:16 +0530

    FundooFrontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA, MatDialogTitle} from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotesComponent } from './component/notes/notes.component';
-import { CreatenoteComponent } from './component/createnote/createnote.component';
-import { UpdatenoteComponent } from './component/updatenote/updatenote.component';
-import { DisplaynoteComponent } from './component/displaynote/displaynote.component';
-import { DeletenoteComponent } from './component/deletenote/deletenote.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { CreatenoteComponent } from './components/createnote/createnote.component';
+import { UpdatenoteComponent } from './components/updatenote/updatenote.component';
+import { DisplaynoteComponent } from './components/displaynote/displaynote.component';
+import { DeletenoteComponent } from './components/deletenote/deletenote.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { SidenavComponent } from './component/sidenav/sidenav.component';
-import { IconlistComponent } from './component/iconlist/iconlist.component';
-import { PinNotesComponent } from './component/pin-notes/pin-notes.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { IconlistComponent } from './components/iconlist/iconlist.component';
+import { PinNotesComponent } from './components/pin-notes/pin-notes.component';
 
 @NgModule({
   declarations: [
@@ -59,9 +59,8 @@ import { PinNotesComponent } from './component/pin-notes/pin-notes.component';
     MatSidenavModule,
     MatSelectModule,
     FlexLayoutModule,
-    MatDialogModule,
+    MatDialogModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ { provide: MatDialogTitle, useValue: {} }, { provide: MatDialogRef, useValue: {} }, { provide: MAT_DIALOG_DATA, useValue: [] } ],  bootstrap: [AppComponent]
 })
 export class AppModule { }
