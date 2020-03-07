5ab4b12d0f961cdfe9b067f0b04a898c8dae3286
Sudeep Katiar <chandanpro32@gmail.com>
2020-03-07 16:43:30 +0530

    FundooFrontend

--- src/app/service/noteservice.service.ts
+++ src/app/service/noteservice.service.ts
@@ -10,87 +10,94 @@ import { tap } from 'rxjs/operators';
 })
 export class NoteserviceService {
   [x: string]: any;
-  private noteURl=environment.notesApiUrl;
+  private noteURl = environment.notesApiUrl;
   private _autoRefresh$ = new Subject();
   private httpOptions = {
-    headers: new HttpHeaders({'Content-Type':'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
   get autoRefresh$() {
     return this._autoRefresh$;
   }
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  addNote(Note:any, token:string):Observable<any> {
-    return this.http.post<any>(this.noteURl + environment.createNoteURL,Note,{headers: new HttpHeaders().set('token', token) }).pipe(tap(() => {
+  addNote(Note: any, token: string): Observable<any> {
+    return this.http.post<any>(this.noteURl + environment.createNoteURL, Note, { headers: new HttpHeaders().set('token', token) }).pipe(tap(() => {
       this._autoRefresh$.next();
     }));
   }
 
-  getAllNotes(token:string):Observable<any> {
-    return this.http.post<any>(this.noteURl + environment.allNoteURL,Note,{headers: new HttpHeaders().set('token', token) });
+  getAllNotes(token: string): Observable<any> {
+    return this.http.post<any>(this.noteURl + environment.allNoteURL, Note, { headers: new HttpHeaders().set('token', token) });
   }
 
   updateNote(Note: any, token: string, id: any): Observable<any> {
     console.log(id);
-    return this.http.post<any>(this.noteURl + environment.updateNoteURL + id,Note,{headers: new HttpHeaders().set('token', token) }).pipe(tap(() => {
+    return this.http.post<any>(this.noteURl + environment.updateNoteURL + id, Note, { headers: new HttpHeaders().set('token', token) }).pipe(tap(() => {
       this._autoRefresh$.next();
     }));
   }
 
-  deleteNote(id:any):Observable<any>{
-    return this.http.post<any>(`${this.noteURl}${environment.deleteNoteURL}${id}`,{},{headers: new HttpHeaders().set('token',  localStorage.token) }) .pipe(tap(() => {
+  deleteNote(id: any): Observable<any> {
+    return this.http.post<any>(`${this.noteURl}${environment.deleteNoteURL}${id}`, {}, { headers: new HttpHeaders().set('token', localStorage.token) }).pipe(tap(() => {
       this._autoRefresh$.next();
     }));
   }
 
-  restoreNote(token:string,id:any):Observable<any>{
-    return this.http.post<any>(this.noteURl + environment.restoreNoteURL + id,{headers: new HttpHeaders().set('token',  token) })
+  restoreNote(token: string, id: any): Observable<any> {
+    return this.http.post<any>(this.noteURl + environment.restoreNoteURL + id, { headers: new HttpHeaders().set('token', token) })
   }
 
-  pinNotes(token:string,id:any):Observable<any>{
+  pinNotes(token: string, id: any): Observable<any> {
 
-    return this.http.post<any>(`${this.noteURl}${environment.pinNoteURL}?id=${id}`,{},{headers: new HttpHeaders().set('token',  localStorage.token) }) .pipe(tap(() => {
+    return this.http.post<any>(`${this.noteURl}${environment.pinNoteURL}?id=${id}`, {}, { headers: new HttpHeaders().set('token', localStorage.token) }).pipe(tap(() => {
       this._autoRefresh$.next();
     }));
   }
 
-  getPinnedNotes(token:string) {
-    return this.http.get<any>(this.noteURl + environment.allPinnedNoteURL,{headers: new HttpHeaders().set('token',  token) });
+  getPinnedNotes(token: string) {
+    return this.http.get<any>(this.noteURl + environment.allPinnedNoteURL, { headers: new HttpHeaders().set('token', token) });
   }
 
-  getUnpinnedNotes(token:string) {
-    return this.http.get<any>(this.noteURl + environment.allUnpinnedNoteURL,{headers: new HttpHeaders().set('token',  token) });
+  getUnpinnedNotes(token: string) {
+    return this.http.get<any>(this.noteURl + environment.allUnpinnedNoteURL, { headers: new HttpHeaders().set('token', token) });
   }
 
   moveToArchiveNote(id: any) {
-    return this.http.post<any>(`${this.noteURl}${environment.archiveNoteURL}/${id}`, {}, { headers: new HttpHeaders().set('token', localStorage.token) }) .pipe(tap(() => {
+    return this.http.post<any>(`${this.noteURl}${environment.archiveNoteURL}/${id}`, {}, { headers: new HttpHeaders().set('token', localStorage.token) }).pipe(tap(() => {
       this._autoRefresh$.next();
     }));
   }
 
-  getArchivedNotes(token:string) {
-    return this.http.get<any>(this.noteURl + environment.allArchivedNoteURL,{headers: new HttpHeaders().set('token', token)});
+  getArchivedNotes(token: string) {
+    return this.http.get<any>(this.noteURl + environment.allArchivedNoteURL, { headers: new HttpHeaders().set('token', token) });
   }
 
-  getReminderNotes(token:string) {
-    return this.http.get<any>(this.noteURl + environment.allReminderNoteURL,{headers: new HttpHeaders().set('token', token)});
+  getReminderNotes(token: string) {
+    return this.http.get<any>(this.noteURl + environment.allReminderNoteURL, { headers: new HttpHeaders().set('token', token) });
   }
 
-  emptyBin(token:string,id:any):Observable<any>{
-    return this.http.post<any>(this.noteURl + environment.emptyBinURL + id,{headers: new HttpHeaders().set('token',  token) })
+  emptyBin(token: string, id: any): Observable<any> {
+    return this.http.post<any>(this.noteURl + environment.emptyBinURL + id, { headers: new HttpHeaders().set('token', token) })
   }
 
-  getBinnedNotes(token:string) {
-    return this.http.get<any>(this.noteURl + environment.allTrashedNoteURL,{headers: new HttpHeaders().set('token', token)});
+  getBinnedNotes(token: string) {
+    return this.http.get<any>(this.noteURl + environment.allTrashedNoteURL, { headers: new HttpHeaders().set('token', token) });
   }
 
-  addColor(id,color){
+  addColor(id, color) {
     console.log(`${environment.notesApiUrl}${environment.colorNoteURL}${id}?color=${color}`);
-    return this.http.post<any>(`${environment.notesApiUrl}${environment.colorNoteURL}${id}?color=${color}`,{}, { headers: new HttpHeaders().set('token', localStorage.token) }).pipe(tap(() => {
+    return this.http.post<any>(`${environment.notesApiUrl}${environment.colorNoteURL}${id}?color=${color}`, {}, { headers: new HttpHeaders().set('token', localStorage.token) }).pipe(tap(() => {
       this._autoRefresh$.next();
     }));
+  }
+
+  addCollaborator(id) {
+    return this.http.post<any>(`${environment.collaboratorApiUrl}${environment.addCollaboratorURL}/${id}`, {}, { headers: new HttpHeaders().set('token', localStorage.token) }).pipe(tap(() => {
+      this._autoRefresh$.next();
+    }));
+
   }
 
 }
