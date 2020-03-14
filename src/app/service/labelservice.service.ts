d378322ce39d4c722681060edfbd5defd7876e0d
Sudeep Katiar <chandanpro32@gmail.com>
2020-03-14 19:27:48 +0530

    FundooFrontend

--- src/app/service/labelservice.service.ts
+++ src/app/service/labelservice.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { HttpserviceService } from './httpservice.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,7 @@ export class LabelserviceService {
   [x: string]: any;
   private labelURl=environment.labelApiUrl;
   private _autoRefresh$ = new Subject();
+  private subject = new Subject<any>();
   private httpOptions = {
     headers: new HttpHeaders({'Content-Type':'application/json'})
   };
@@ -21,7 +24,11 @@ export class LabelserviceService {
 
   private labelsList = new Subject<any>();
   
-  constructor(private http:HttpClient) { }
+  constructor(private router: Router, private http:HttpClient) { }
+
+  getLabels(): Observable<any> {
+    return this.subject.asObservable();
+  }
 
   setlabelList(message:any){
 
@@ -32,8 +39,12 @@ getNoteLabels(noteId:any){
   return this.http.get(`${environment.labelApiUrl}/${environment.getLabels}?noteId=${noteId}`,{headers: new HttpHeaders().set('token',  localStorage.token) });
 }
 
-getAllLabels(){
+getAllLabels():Observable<any>{
   return this.http.get(`${environment.labelApiUrl}${environment.getLabelsList}`, { headers: new HttpHeaders().set('token', localStorage.token)});
+}
+
+createLabel(name: any){
+  return this.http.post<any>(`${environment.labelApiUrl}${environment.createLabelUrl}`, name, { headers: new HttpHeaders().set('token', localStorage.token) });
 }
 
 }
